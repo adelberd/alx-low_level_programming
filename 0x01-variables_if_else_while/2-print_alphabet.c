485b65688cf92ea3658b9ca5034e8f5b8e9aa2db
adelberd <motlhabinyana.am@gmail.com>
2023-07-14 23:52:53 +0000

    Task 2, use putchar to print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main block function
+* Return: 0
+*/
+
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int a = 26;
+
+	for (a = 0; a < 26; a++)
+	{
+		putchar(alpha[a]);
+		putchar("\n");
+	}
+	return (0);
+}
