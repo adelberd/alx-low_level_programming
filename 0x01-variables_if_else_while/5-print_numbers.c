8ae2eed437f2ae50d9b8af5d879388f780fedd94
adelberd <motlhabinyana.am@gmail.com>
2023-07-15 00:56:14 +0000

    Task 5, print digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main block function
+* Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
