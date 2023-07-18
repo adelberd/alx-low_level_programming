dc98a891b929b0271ff5ccf664d356f0a0ff072b
adelberd <motlhabinyana.am@gmail.com>
2023-07-18 19:44:28 +0000

    Task 0, printing _putchar to the screen

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-	char _putchar[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	char name = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		putchar(_putchar[i]);
+		_putchar(name[i]);
 	}
 
 	putchar('\n');
