039352f1dd40525a8b7a632ffbad3c15731d093b
adelberd <motlhabinyana.am@gmail.com>
2023-07-19 18:17:46 +0000

    Task 5 -  0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_sign - print +, 0 or - when n is greater, equal or less
+*              than zero
+* @n: n - is a number
+* Return: 1, 0 or -1 respectively
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+
+	_putchar('-');
+	return (-1);
+}
