472fcaef477edc704dc914b2bc7cd6c1461b335f
adelberd <motlhabinyana.am@gmail.com>
2023-07-28 21:39:44 +0000

    Fixed task 3 - 0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -20,9 +20,9 @@ int _strcmp(char *s1, char *s2)
 	}
 
 	if (*s1 != '\0' || *s2 != '\0')
-		return (s1 - s2);
+		return (s2 - s1);
 	if (flag == 0)
 		return (0);
 	else
-		return (s1 - s2);
+		return (s2 - s1);
 }
