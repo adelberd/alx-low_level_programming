f60c6d91ad4f132c7a374e9257d6bb357bc8e716
adelberd <motlhabinyana.am@gmail.com>
2023-08-01 22:19:18 +0000

    Task 3 - 0x07-pointers_arrays_strings (3-strspn.c)

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,4 +9,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	int i;
+	unsigned int n = 0;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				n++;
+				break;
+			} else if (accept[i + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
 }
