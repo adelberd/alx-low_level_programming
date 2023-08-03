c87dc2120b7d809b1ee2c3efc90ef39a9d13ac15
adelberd <motlhabinyana.am@gmail.com>
2023-08-03 09:02:54 +0000

    Task 4 - 0x08-recursion (4-pow_recursion.c)

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*
-*
-*
-*
+* _pow_recursion - print the power of number raised to another
+*		 use recursive function.
+* @x: base integer value
+* @y: power integer value
+* Return: returns integer
 */
 
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
 
+	return (x * _pow_recursion(x, (y - 1)));
+}
