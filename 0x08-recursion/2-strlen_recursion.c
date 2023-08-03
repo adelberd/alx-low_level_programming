5c9b160b5ba62a91c1902d4cd99597ab5d59025e
adelberd <motlhabinyana.am@gmail.com>
2023-08-03 07:52:51 +0000

    Task 2 - 0x08-recursion (2-strlen_recursion.c)

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*
-*
-*
+* _strlen_recursion - print length of a string recursively
+* @s: string to evaluate
+* Return: integer value i.e the length of a string
 *
 */
 
-
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	else if (s == NULL)
+		return (0);
+}
