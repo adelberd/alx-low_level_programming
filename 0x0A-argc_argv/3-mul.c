dd3759c3d654c772b4b7f7017882f403c1ae10b8
adelberd <motlhabinyana.am@gmail.com>
2023-08-09 09:25:37 +0000

    Task 3 - 0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,13 +2,24 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-* main - prints all arguments passed to main
+* main - multiplies two numbers
 * @argc: number of arguments.
 * @argv: array of pointer to arguments from main
 * Return: 0 success
 */
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
+	int mul;
 
+	if (argc == 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	mul = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", mul);
+	return (0);
 }
