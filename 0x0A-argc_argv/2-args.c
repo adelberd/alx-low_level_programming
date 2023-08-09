df9489daaf869ede814a3fd2b6012d979c557bd4
adelberd <motlhabinyana.am@gmail.com>
2023-08-09 09:14:42 +0000

    Task 2 - 0x0A-argc_argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,8 +2,22 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-*
-*
-*
-* Return: 
+* main - prints all arguments passed to main
+* @argc: number of arguments.
+* @argv: array of pointer to arguments from main
+* Return: 0 success
 */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%s\n", argv[0]);
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+			printf("%s\n", argv[i]);
+	}
+	return (0);
+}
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-*
-*
-*
-* Return: 
+* main - prints all arguments passed to main
+* @argc: number of arguments.
+* @argv: array of pointer to arguments from main
+* Return: 0 success
 */
+
+int main(int argc, char * argv[])
+{
+
+}
