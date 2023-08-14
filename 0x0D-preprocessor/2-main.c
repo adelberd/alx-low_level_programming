a8176895245fc8a435eccfc99edc9ae924319e76
adelberd <motlhabinyana.am@gmail.com>
2023-08-14 12:35:56 +0000

    Task 2 - 0x0D-preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+* main - prints the name of the file it was compiled from.
 *
-*
-*
-*
+* Return: 0 for success
 */
 
-
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
