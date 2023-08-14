8c44541aca6b64dfde18f6bcba4d483bb475f978
adelberd <motlhabinyana.am@gmail.com>
2023-08-14 13:26:18 +0000

    Task 3 and Task 4 - 0x0D-preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,9 @@
-#ifndef ABS(x)
+#ifndef ABS_H
+#define ABS_H
+/*
+*  func-like macro, that computes the absolute value of a number x
+* @x: number to convert to absolute value
+*/
 #define ABS(x) ((x) < 0 ? ((x) * -1) : ((x) * 1))
 
-#endif /* ABS */
+#endif /* ABS_H */
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,11 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+#ifndef SUM_H
+#define SUM_H
 /**
-*
-*
-*
-*
+* function-like macro, that computes the sum of the numbers x and y
+* @x: 1st number.
+* @y: 2nd number.
 */
+#define SUM(x, y) ((x) + (y))
 
-
+#endif /* SUM_H */
