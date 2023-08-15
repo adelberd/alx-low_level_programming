99d2cf882cdac59e0cf6d7fd5e433b8e804f2c5d
adelberd <motlhabinyana.am@gmail.com>
2023-08-15 13:22:11 +0000

    Task 0 - 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - dog with a name, agae and owner's name
+* @name: the name of the dog
+* @age: the age of the dog
+* @owner: the dog's owner's name
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
