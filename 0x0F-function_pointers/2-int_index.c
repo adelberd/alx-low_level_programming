e94480352f5166ec86c4fd0b1ab1bca294888ac3
adelberd <motlhabinyana.am@gmail.com>
2023-08-18 18:40:27 +0000

    Task 2 - 0x0F-function_pointers (Hot fix: fixed if statement condition)

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,14 +11,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i;
+	int i;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		if (cmp(array[i]) != 0)
 			return (i);
 	}
 	return (-1);
