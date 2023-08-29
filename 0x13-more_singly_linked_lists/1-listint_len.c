d1024f9ace4679b5232424ffdcc626bc210757a9
adelberd <motlhabinyana.am@gmail.com>
2023-08-29 15:41:51 +0000

    Task 1 - 0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* listint_len - get the number of elements in linked list.
+* @h: pointer to the struct node.
+*
+* Return: number of elements in a linked list.
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	int count_elem = 0;
+
+	while (h != NULL)
+	{
+		count_elem += 1;
+		h = h->next;
+	}
+}
