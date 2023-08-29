e97feef1960fc4a78b7aaa7aad941922b3a475dd
adelberd <motlhabinyana.am@gmail.com>
2023-08-29 19:10:20 +0000

    Task 3 - 0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+* add_nodeint_end - add new node at the end of a linked list.
+* @head: pointer to pointer to new node.
+* @n: data to add at the end of a linked list.
+*
+* Return: address of a new element.
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *traverse, *add_node;
+
+	if (add_node == NULL)
+		return (NULL);
+
+	add_node->n = n;
+	add_node->next = NULL;
+
+	if (*head == NULL)
+		*head = add_node;
+	else
+	{
+		traverse = *head;
+		while (traverse->next != NULL)
+			traverse = traverse->next;
+
+		traverse->next = add_node;
+	}
+}
