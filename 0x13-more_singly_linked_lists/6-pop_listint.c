3071ee73315e4f45d31afe76a0c3a7d7f6718076
adelberd <motlhabinyana.am@gmail.com>
2023-08-30 17:44:36 +0000

    Task 6 (Hot fix: free temp pointer) - 0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,12 +10,15 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int data;
 
 	if (*head == NULL)
 		return (0);
 
 	temp = *head;
 	(*head) = (*head)->next;
+	data = temp->n;
+	free(temp);
 
-	return (temp->n);
+	return (data);
 }
